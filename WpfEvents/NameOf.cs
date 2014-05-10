bde0bbf7b860d9f5581d05c33b4561e57299be97
Johan Larsson <johan_gavle1@hotmail.com>
2014-05-10 17:53:44 +0200

    NameOf

---
+++ WpfEvents/NameOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEvents
+{
+    using System.Linq.Expressions;
+
+    public static class NameOf
+    {
+        public static string Event<T>(Action<T> @event)
+        {
+            string name = @event.Method.Name;
+            throw new NotImplementedException("message");
+        }
+    }
+}
